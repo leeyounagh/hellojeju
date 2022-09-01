c4d14a911ca9341ade940f075a240778ca233d29
leesuyoen <dudgk0216@naver.com>
2022-09-01 22:05:42 +0900

    배포후 수정중

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -32,18 +32,18 @@ const NavBar = (props) => {
    
     if(user.userData&&!user.userData.isAuth){
         return (
-            <div className="nav_text2" style={{position:'fixed',top:'0px',width:'100%',height:'60px',
+            <div className="nav_text2" style={{position:'fixed',top:'0px',width:'100%',height:'10%',
             left:'0px', display:'flex', justifyContent:'space-around',padding:'20px',zIndex:'300',
             background:'#94b1b9'  }}>
                 <div>
                 <a href='/landing' className="nav_text" style={{marginRight:'10px',marginTop:"30px",color:'#f3efe6',
-                  fontSize:"18px",fontWeight:'900',position:'absolute',left:'200px',top:'-15px'}}>Home</a>
+                  fontSize:"18px",fontWeight:'900',}}>Home</a>
                 </div>
                 <div>
                 <a href='/register' className="nav_text" style={{marginRight:'10px',marginTop:"30px",color:'#f3efe6',
-                   fontSize:"18px",fontWeight:'900',position:'absolute',left:'810px',top:'-15px'}}>Register</a>
+                   fontSize:"18px",fontWeight:'900',}}>Register</a>
                 <a href='/login' className="nav_text" style={{marginRight:'10px',marginTop:"30px",color:'#f3efe6',   
-                  fontSize:"18px",fontWeight:'900',position:'absolute',left:'900px',top:'-15px'}}>Login</a>
+                  fontSize:"18px",fontWeight:'900',}}>Login</a>
                 </div>
            
                
@@ -56,21 +56,21 @@ const NavBar = (props) => {
         background:'#94b1b9' }}>
               <div>
               <a href='/landing' className="nav_text"style={{marginRight:'10px',marginTop:"20px",color:'#f3efe6',weight:'50',
-              fontSize:"18px",fontWeight:'900',position:'absolute',left:'200px',top:'-10px'
+              fontSize:"18px",fontWeight:'900'
            }}>Home</a>
               </div>
                 <div>
                 <a href='/travelspot' className="nav_text" style={{marginRight:'30px',marginTop:"50px",color:'#f3efe6',weight:'50',
-              fontSize:"18px",fontWeight:'900',position:'absolute',left:'560px',top:'-38px'}}>TravelSpot</a>
+              fontSize:"18px",fontWeight:'900'}}>TravelSpot</a>
               
                 <a href='/travelnews' className="nav_text" style={{marginRight:'30px',marginTop:"30px",color:'#f3efe6',weight:'50',
-              fontSize:"18px",fontWeight:'900',position:'absolute',left:'670px',top:'-18px'}}>News</a>
+              fontSize:"18px",fontWeight:'900'}}>News</a>
              
                 <a href='/community'  className="nav_text"  style={{marginRight:'30px',marginTop:"30px",color:'#f3efe6',weight:'50',
-              fontSize:"18px",fontWeight:'900',position:'absolute',left:'730px',top:'-18px'}}>community</a>
+              fontSize:"18px",fontWeight:'900'}}>community</a>
                   
                   <a href='/userstyle'  className="nav_text"  style={{marginRight:'30px',marginTop:"50px",color:'#f3efe6',weight:'70',
-              fontSize:"18px",fontWeight:'900',color:'#f58d9c',position:'absolute',left:'845px',top:'-32px'}}><BsHeartFill></BsHeartFill></a>
+              fontSize:"18px",fontWeight:'900',color:'#f58d9c'}}><BsHeartFill></BsHeartFill></a>
               
 
                 
