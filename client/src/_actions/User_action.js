98dd6faefec4247bb3a862f29f2ac9d59ca89dbd
leesuyoen <dudgk0216@naver.com>
2022-09-05 17:57:30 +0900

    디비연결 확인

--- client/src/_actions/User_action.js
+++ client/src/_actions/User_action.js
@@ -15,7 +15,7 @@ import {
 
 export function loginUser(dataToSubmit) {
 
-    const request = axios.post('/api/users/login', dataToSubmit)
+    const request = axios.post(' https://hellojeju.herokuapp.com/api/users/login', dataToSubmit)
         .then(response => response.data)
 
     return {
