eb49901afd3ef66c34d24f71724e3b15e80c7301
lee <dudgk0216@naver.com>
2022-09-06 19:48:00 +0900

    테스트

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://hellojeju.herokuapp.com/',
+      target: 'https://hellojeju.herokuapp.com/api/',
       changeOrigin: true,
     })
   );
